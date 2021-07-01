1057021def61e873a11c228216284570ed3f589b
KorolevaVeraAleksanrovna <86655014+KorolevaVeraAleksanrovna@users.noreply.github.com>
2021-07-01 11:02:15 +0400

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -2,5 +2,9 @@
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+longest_name = ""
+for name in names:
+    if len(name) > len(longest_name):
+        longest_name = name
 
-# TODO: your code here
+print(longest_name)
